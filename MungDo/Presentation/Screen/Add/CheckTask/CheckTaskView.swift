263e5c16425e3562435e4b7daf97b4b499d61e51
Ito <ckdwo23@gmail.com>
2025-06-04 11:23:48 +0900

    feat: CheckTaskView에 캘린더 추가

--- MungDo/Presentation/Screen/Add/CheckTask/CheckTaskView.swift
+++ MungDo/Presentation/Screen/Add/CheckTask/CheckTaskView.swift
@@ -12,21 +12,37 @@ struct CheckTaskView: View {
     @State private var showTaskFlow = false
     
     var body: some View {
-        VStack(spacing: 30) {
-            Text("Check Task View")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-
-            Text("태스크를 체크하는 화면입니다")
-                .font(.body)
-                .foregroundColor(.gray)
-            
-            Button(action: { showTaskFlow = true }) {
-                CustomButtonLabel(title: "추가하기")
+        HStack{
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 680 , height: 680)
+                    .background(.white)
+                    .cornerRadius(20)
+                TestCalendarView()
+                    .frame(width: 656 , height: 620)
             }
-            
+            Spacer()
+            VStack(spacing: 30) {
+                Text("Check Task View")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+
+                Text("태스크를 체크하는 화면입니다")
+                    .font(.body)
+                    .foregroundColor(.gray)
+                
+                Spacer()
+                
+                Button(action: { showTaskFlow = true }) {
+                    CustomButtonLabel(title: "추가하기")
+                }
+                
+               
+            }
             Spacer()
         }
+        
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding(55)
         .background(Color.backgroundPrimary)
