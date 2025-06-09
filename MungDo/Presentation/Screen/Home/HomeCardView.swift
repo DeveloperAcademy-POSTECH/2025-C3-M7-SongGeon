78f563027ac5f794a84c7da46a13c35636553588
HwangJeha <155410554+Jeha0101@users.noreply.github.com>
2025-06-09 22:08:07 +0900

    feat: Home View 수정
    - 불필요한 주석 삭제
    - 커스텀 폰트 적용
    - 폰트 색상 적용

--- MungDo/Presentation/Screen/Home/HomeCardView.swift
+++ MungDo/Presentation/Screen/Home/HomeCardView.swift
@@ -14,40 +14,31 @@ struct HomeCardView: View {
     
     var body: some View {
         
-            VStack(spacing: 16){
-                ZStack{
-                    RoundedRectangle(cornerRadius:20)
-                            .fill(Color("ButtonPrimary"))
-                            //.frame(height: 200)
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        //.frame(height: 150)
-                        
-                    
-                }
-                .frame(width: 400, height: 400)
-                .padding(.horizontal,16)
-               
-                Text(title)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 48)
+        VStack(spacing: 16){
+            ZStack{
+                RoundedRectangle(cornerRadius:20)
+                    .fill(Color("ButtonPrimary"))
+                image
+                    .resizable()
+                    .scaledToFit()
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                RoundedRectangle(cornerRadius: 50)
-                    .fill(Color("ButtonSecondary"))
-                    .shadow(color: Color.buttonPrimary.opacity(0.4), radius: 20, x: 4, y: 4)
-                    
-            )
-            .padding(.vertical, 32)
-            .padding(16)
+            .frame(width: 400, height: 400)
+            .padding(.horizontal,16)
             
-        
-        
+            Text(title)
+                .font(.headingFont)
+                .foregroundColor(.primary02)
+                .multilineTextAlignment(.center)
+                .padding(.top, 48)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            RoundedRectangle(cornerRadius: 50)
+                .fill(Color("ButtonSecondary"))
+                .shadow(color: Color.buttonPrimary.opacity(0.4), radius: 20, x: 4, y: 4)
+            
+        )
+        .padding(.vertical, 32)
+        .padding(16)
     }
-        
 }
--- MungDo/Presentation/Screen/Home/HomeView.swift
+++ MungDo/Presentation/Screen/Home/HomeView.swift
@@ -14,34 +14,20 @@ struct HomeView: View {
             ZStack {
                 Color.backgroundPrimary.edgesIgnoringSafeArea(.all)
                 VStack(spacing: 50) {
-
-                    
                     HStack() {
                         NavigationLink(destination: CheckTaskView()) {
                             HomeCardView(image:Image("Home/home_add"), title:"날짜별로 모아보기")
                         }
-                        
                         NavigationLink(destination: CompletedTaskView()) {
-//                            Text("Todo List")
-//                                .font(.title2)
-//                                .foregroundColor(.white)
-//                                .frame(width: 200, height: 50)
-//                                .background(Color.green)
-//                                .cornerRadius(10)
                             HomeCardView(image:Image("Home/home_look"), title:"오늘 할 일 열어놓기")
-
                         }
                     }
                     .padding(.horizontal, 48)
                 }
             }
-            //.navigationTitle("Home")
         }
     }
 }
-
-
-
 
 #Preview {
     HomeView()
